e5198b06e5ce2cf1c2c4249c9cd7dcf63825e520
vinicius <vinicius.cardoso8888@outlook.com>
2022-06-29 19:46:50 -0300

    complementando aplicação

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSom_pom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSom_clap (){
+    document.querySelector('#som_tecla_clap').play();
+}   
+const listaDeTeclas = document.querySelectorAll('.tecla');//criando uma contante para receber o querySelector
+
+// document.querySelector('.tecla_pom').onclick = tocaSom_pom; selecionado o class tecla_pom
+
